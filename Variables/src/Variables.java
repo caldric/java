b8fdaf5deb820a75699bdb60f31cbfff6064bba2
caldric <cvaldric@gmail.com>
2021-01-08 05:24:37 -0500

    Explore the int data type

---
+++ Variables/src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        int firstNumber = 5;
+        System.out.println(firstNumber);
+
+        firstNumber = 10 + 5;
+        System.out.println(firstNumber);
+
+        int secondNumber = 12;
+        System.out.println(secondNumber);
+
+        int thirdNumber = 6;
+        System.out.println(thirdNumber);
+
+        int sum = firstNumber + secondNumber + thirdNumber;
+        System.out.println(sum);
+
+        int last = 1000 - sum;
+        System.out.println(last);
+    }
+}
